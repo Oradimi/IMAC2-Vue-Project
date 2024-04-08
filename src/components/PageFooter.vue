59ccbf81a3fb51bacf81c9fd028088ce5e00776a
Oradimi <loganargouse@gmail.com>
2024-04-08 15:00:44 +0200

    Fix page bounds

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="pagination">
-    <button @click="updatePage(-1)" :disabled="currentPage === 1">Previous</button>
+    <button @click="updatePage(-1)" :disabled="currentPage <= 1">Previous</button>
     <span>{{ currentPage }} / {{ totalPages }}</span>
-    <button @click="updatePage(+1)" :disabled="currentPage === totalPages">Next</button>
+    <button @click="updatePage(+1)" :disabled="currentPage >= totalPages">Next</button>
 </div>
 </template>
   
